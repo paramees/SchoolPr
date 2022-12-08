fd9f6594c5689c60f6ae0e7d71a0c69a64e9a0b8
paramees <sergepal2003@gmail.com>
2022-12-09 01:13:54 +0200

    Done 1.1 and some part of 1.2

---
+++ 1.2/go2.js
@@ -0,0 +1,45 @@
+// этот файл надо будет дописать...
+
+// не обращайте на эту функцию внимания 
+// она нужна для того чтобы правильно читать входные данные
+function readHttpLikeInput(){
+    var fs = require("fs");
+    var res = "";
+    var buffer = Buffer.alloc ? Buffer.alloc(1) : new Buffer(1);
+    let was10 = 0;
+    for(;;){ 
+        try { fs.readSync(0 /*stdin fd*/, buffer, 0, 1); } catch (e) {break; /* windows */}
+        if(buffer[0] === 10 || buffer[0] === 13) {
+            if (was10 > 10) 
+                break;
+            was10++;
+        } else 
+           was10 = 0;
+        res += new String(buffer);
+    }
+
+    return res;
+}
+
+let contents = readHttpLikeInput();
+
+// вот эту функцию собственно надо написать
+function parseTcpStringAsHttpRequest(string) { 
+    allAndBody = string.trim().split("\n\n")
+    splitedStr = allAndBody[0].trim().split("\n")
+    methodAndUri = splitedStr[0].split(" ")
+    head = {}
+    for (let i = 1; i < splitedStr.length; i++) {
+        str = splitedStr[i].split(": ")
+        head[str[0]] = str[1]
+    }
+  return { 
+    method: methodAndUri[0], 
+    uri : methodAndUri[1],
+    headers:  head, 
+    body : allAndBody[1], 
+  }; 
+}
+
+http = parseTcpStringAsHttpRequest(contents); 
+console.log(JSON.stringify(http, undefined, 2));
