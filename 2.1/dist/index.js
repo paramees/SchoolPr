2da1d6e9cb992fef3dbfa2d392749082db8f9b1b
paramees <sergepal2003@gmail.com>
2023-01-31 22:12:51 +0200

    done 2.1

--- 2.1/dist/index.js
+++ 2.1/dist/index.js
@@ -1,2 +1,72 @@
 "use strict";
-console.log("hiiiii");
+// 1. 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function getFirstWord(a) {
+    return a.split(/ +/)[0].length;
+}
+// 2. 
+function getUserNamings(a) {
+    return {
+        fullname: a.name + " " + a.surname,
+        initials: a.name[0] + "." + a.surname[0]
+    };
+}
+// 3. 
+// <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining>
+function getAllProductNames(a) {
+    var _a;
+    return ((_a = a === null || a === void 0 ? void 0 : a.products) === null || _a === void 0 ? void 0 : _a.map(prod => prod === null || prod === void 0 ? void 0 : prod.name)) || [];
+}
+class Cat {
+    constructor(_name, bool) {
+        this._name = _name;
+        this.bool = bool;
+    }
+    name() { return this._name; }
+}
+class Dog {
+    constructor(_name, num) {
+        this._name = _name;
+        this.num = num;
+    }
+    name() { return this._name; }
+}
+hey({ name: () => "roma", cuteness: 100 });
+hey({ name: () => "vasya", coolness: 100 });
+// 4.2
+let a = new Cat("myavchik", true);
+let b = new Dog("gavchik", 333);
+hey(a);
+hey(b);
+// 4.3
+function hey(a) {
+    return "hey! i'm " + a.name()
+        + (a.type === "cat" ? ("cuteness: " + a.cuteness) : ("coolness: " + a.coolness));
+}
+hey({ name: () => "roma", type: "cat", cuteness: 100 });
+hey({ name: () => "vasya", type: "dog", coolness: 100 });
+// 5.
+// google for Record type
+function stringEntries(a) {
+    return Array.isArray(a) ? a : Object.keys(a);
+}
+// 6.
+// you don't know Promises and async/await yet. Or do you? 
+// ....can be hard, don't worry and SKIP if you do not know how to do it
+function world(a) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "*".repeat(a);
+    });
+}
+const hello = () => __awaiter(void 0, void 0, void 0, function* () {
+    return yield world(10);
+});
+hello().then(r => console.log(r)).catch(e => console.log("fail"));
--- 2.1/src/index.ts
+++ 2.1/src/index.ts
@@ -1,1 +1,82 @@
-console.log("hiiiii")
+// 1. 
+
+function getFirstWord(a: string) {
+	return a.split(/ +/)[0].length;
+}
+
+// 2. 
+
+function getUserNamings(a: {name: string, surname: string}) {
+  return { 
+		fullname: a.name + " " + a.surname, 
+		initials: a.name[0] + "." + a.surname[0] 
+	};
+}
+
+// 3. 
+
+// <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining>
+function getAllProductNames(a: {products: {name: string}[]}) {
+  return a?.products?.map(prod => prod?.name) || [];
+}
+
+// 4.1
+
+// easy way is using 'as' keyword
+// hard way is ?...
+
+interface Pet {
+    name: () => string,
+    type? : "cat" | "dog",
+    cuteness? : number
+    coolness? : number
+}
+
+class Cat implements Pet {
+    constructor(public _name: string, public bool? : boolean) {}
+    name() : string {return this._name} 
+}
+
+class Dog implements Pet {
+    constructor(public _name: string, public num? : number) {}
+    name() : string {return this._name} 
+}
+
+hey({name: () => "roma", cuteness: 100})
+hey({name: () => "vasya", coolness: 100})
+
+// 4.2
+
+let a = new Cat("myavchik", true)
+let b = new Dog("gavchik", 333)
+hey(a)
+hey(b)
+
+// 4.3
+
+function hey(a: Pet) {
+    return "hey! i'm " + a.name()
+		 + (a.type === "cat" ? ("cuteness: "+a.cuteness) : ("coolness: "+a.coolness))
+}
+hey({name: () => "roma", type: "cat", cuteness: 100})
+hey({name: () => "vasya", type: "dog", coolness: 100})
+
+// 5.
+
+// google for Record type
+function stringEntries(a:Record<number | string, string>) {
+   return Array.isArray(a) ? a : Object.keys(a)
+}
+
+// 6.
+
+// you don't know Promises and async/await yet. Or do you? 
+// ....can be hard, don't worry and SKIP if you do not know how to do it
+
+async function world(a:number) {
+    return "*".repeat(a)
+}
+const hello = async () => {
+   return await world(10)
+}
+hello().then(r => console.log(r)).catch(e => console.log("fail"))
