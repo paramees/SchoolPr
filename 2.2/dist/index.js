9d22852d64addedcb79878e41e7ebade7e07249f
paramees <sergepal2003@gmail.com>
2023-02-09 17:51:56 +0200

    2.2 done!

--- 2.2/dist/index.js
+++ 2.2/dist/index.js
@@ -21,14 +21,83 @@ function fetchIp() {
 }
 //fetchIp().then(ip => console.log(ip.ip))
 //3.1
-function randomNames1() {
-    Promise.all([getRandName(), getRandName(), getRandName()]).then((data) => console.log(data));
-}
 function getRandName() {
     return __awaiter(this, void 0, void 0, function* () {
         return yield (yield fetch("https://random-data-api.com/api/name/random_name")).json();
     });
 }
-randomNames1();
+function randomNames1() {
+    Promise.all([getRandName(), getRandName(), getRandName()]).then((data) => console.log(data.length));
+}
+//randomNames1()
 //3.2
+function randomNames2() {
+    for (let i = 0; i < 3; i++) {
+        getRandName().then((data) => console.log(data));
+    }
+}
+//randomNames2()
 //3.3
+function randomNames3() {
+    let getName;
+    for (let i = 0; i < 3; i++) {
+        getName = fetch("https://random-data-api.com/api/name/random_name").then((resp) => resp.json());
+        getName.then((data) => console.log(data)).catch((e) => console.error(e));
+    }
+}
+//randomNames3()
+//4.1
+function getWomen() {
+    let getUser;
+    getUser = fetch("https://random-data-api.com/api/users/random_user").then((resp) => resp.json());
+    getUser.then((user) => {
+        if (user.gender = "Female") {
+            console.log(user);
+        }
+        else {
+            getWomen();
+        }
+    });
+}
+//getWomen()
+//4.2
+function getUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield (yield fetch("https://random-data-api.com/api/users/random_user")).json();
+    });
+}
+function getWomenAsync() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let user;
+        do {
+            user = yield (yield fetch("https://random-data-api.com/api/users/random_user")).json();
+        } while (user.gender != "Female");
+        console.log(user);
+    });
+}
+//getWomenAsync()
+//5
+function f52() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let ip = fetchIp().then((data) => {
+            f51(() => console.log(data.ip));
+        });
+    });
+}
+function f51(callback) {
+    callback();
+}
+// (async () => {
+//     await f52()
+// })()
+//6
+function f61() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let ip = yield fetchIp().then((data) => data.ip);
+        return ip;
+    });
+}
+function f62(callback) {
+    f61().then((ip) => callback(ip));
+}
+f62((ip) => console.log(ip));
--- 2.2/src/index.ts
+++ 2.2/src/index.ts
@@ -17,14 +17,86 @@ async function getRandName() : Promise<object> {
 }
 
 
-function randomNames1 () {
+function randomNames1 (): void {
     Promise.all([getRandName(), getRandName(), getRandName()]).then((data : object[]) => console.log(data.length));
 }
 
 
-randomNames1()
+//randomNames1()
 //3.2
+function randomNames2 (): void {
+    for(let i = 0; i < 3; i++) {
+    getRandName().then((data : object) => console.log(data));
+    }
+}
 
+//randomNames2()
 
 //3.3
+function randomNames3 () : void {
+    let getName;
+    for(let i = 0; i < 3; i++) {
+        getName = fetch("https://random-data-api.com/api/name/random_name").then((resp: Response) => resp.json());
+        getName.then((data: Object) => console.log(data)).catch((e: Error) => console.error(e));
+    }
+}
 
+//randomNames3()
+
+//4.1
+function getWomen () : void {
+    let getUser;
+    getUser = fetch("https://random-data-api.com/api/users/random_user").then((resp: Response) => resp.json());
+    getUser.then((user: {"gender" : string}) => {
+        if (user.gender = "Female") {
+            console.log(user)
+        } else {
+            getWomen()
+        }
+    })
+}
+
+//getWomen()
+
+//4.2
+async function getUser() : Promise<any> {
+    return await (await fetch("https://random-data-api.com/api/users/random_user")).json()
+}
+
+async function getWomenAsync () : Promise<void> {
+    let user: {"gender" : string} 
+    do {
+        user = await (await fetch("https://random-data-api.com/api/users/random_user")).json();
+    } while (user.gender != "Female")    
+    console.log(user)
+}
+
+//getWomenAsync()
+
+//5
+async function f52 () : Promise<any> {
+    let ip = fetchIp().then((data: {"ip": string}) => {
+        f51(() => console.log(data.ip))
+    })
+}
+
+function f51 (callback : Function) {
+    callback()
+}
+
+
+// (async () => {
+//     await f52()
+// })()
+
+//6
+async function f61 () : Promise<string> {
+    let ip : string = await fetchIp().then((data: {"ip": string}) => data.ip)
+    return ip
+}
+
+function f62 (callback: (ip:string) => void) : void {
+    f61().then((ip) => callback(ip))
+}
+
+f62((ip) => console.log(ip));
