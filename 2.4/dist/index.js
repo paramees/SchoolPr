ab61432f2646f91c415fdce1cdc1d1a54022635e
paramees <sergepal2003@gmail.com>
2023-02-20 23:49:48 +0200

    Done multiuser v1

--- 2.4/dist/index.js
+++ 2.4/dist/index.js
@@ -1,15 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongodb_1 = require("mongodb");
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 const app = (0, express_1.default)();
 const PORT = 5001;
-const URI = "mongodb+srv://Sergey:Passw123@cluster0.4zcsikv.mongodb.net/Todo?retryWrites=true&w=majority";
-let lastID = 1;
+const URI = "mongodb+srv://Sergey:Passw12343@cluster0.4zcsikv.mongodb.net/Todo?retryWrites=true&w=majority";
 let dbList; //data base with to do list
 const client = new mongodb_1.MongoClient(URI);
 //connectin to db
@@ -20,40 +30,115 @@ client.connect()
     app.listen(PORT, () => console.log(`Running on PORT ${PORT}`)); //after db connect starts server
 })
     .catch((err) => console.log("Mongo error " + err));
+app.use(session({
+    store: new FileStore({}),
+    key: "user_session",
+    secret: 'strong password',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 100000 },
+}));
 app.use(express_1.default.static(__dirname.replace("dist", "html")));
 app.use(bodyParser.json());
 app.route("/api/v1/items")
     .get(function (request, response) {
-    dbList.find({}, { projection: { _id: 0 } }).sort({ id: 1 }).toArray()
-        .then((data) => {
-        console.log(data);
-        lastID = data.length == 0 ? 0 : data[data.length - 1].id;
-        console.log(lastID);
-        response.send(JSON.stringify({ items: data }));
+    dbList.findOne({ login: request.session.login }, { projection: { _id: 0 } }).then(user => {
+        if (user && user.items) {
+            response.send(JSON.stringify({ items: user.items }));
+        }
+        else {
+            response.send(JSON.stringify({ items: [] }));
+        }
     });
 })
     .post(function (request, response) {
-    let data = request.body;
-    dbList.insertOne({
-        id: ++lastID,
-        text: data.text,
-        checked: false
-    }).then((res) => {
-        response.send(JSON.stringify({ id: lastID }));
+    return __awaiter(this, void 0, void 0, function* () {
+        let data = request.body.text;
+        let id;
+        let user = yield dbList.findOne({ login: request.session.login });
+        if (user) {
+            if (user.items) {
+                id = user.items.reduce((acc, curr) => acc.id > curr.id ? acc : curr);
+            }
+            else {
+                id = 1;
+            }
+        }
+        else {
+            response.status(400).send(JSON.stringify({ "error": "User is not exist!" }));
+            return;
+        }
+        dbList.updateOne({ login: request.session.login }, {
+            $push: {
+                "items": {
+                    "id": id,
+                    "text": data,
+                    "checked": false
+                }
+            }
+        }).then((res) => {
+            response.send(JSON.stringify({ id: id }));
+        });
     });
 })
     .put(function (request, response) {
     let data = request.body;
-    dbList.findOneAndUpdate({ id: data.id }, { $set: {
-            text: data.text,
-            checked: data.checked
-        } }).then((res) => {
-        response.send(JSON.stringify({ "ok": Boolean(res.ok) }));
+    dbList.updateOne({ login: request.session.login, "items.id": data.id }, {
+        $set: {
+            "items.$.text": data.text,
+            "items.$.checked": data.checked
+        }
+    }).then((res) => {
+        response.send(JSON.stringify({ "ok": true }));
     });
 })
     .delete(function (request, response) {
-    dbList.deleteOne({ id: request.body.id })
+    dbList.updateOne({ login: request.session.login }, {
+        $unset: {
+            items: {
+                id: request.body.id
+            }
+        }
+    })
         .then((res) => {
-        response.send(JSON.stringify({ "ok": Boolean(res.deletedCount) }));
+        response.send(JSON.stringify({ "ok": true }));
+    });
+});
+app.route("/api/v1/login")
+    .post(function (request, response) {
+    let data = request.body;
+    dbList.findOne({ login: data.login }).then(user => {
+        if (user) {
+            request.session.login = data.login;
+            response.send(JSON.stringify({ "ok": true }));
+        }
+        else {
+            response.status(403).send(JSON.stringify({ "error": "User is not exist!" }));
+        }
+    });
+});
+app.route("/api/v1/logout")
+    .post(function (request, response) {
+    request.session.destroy;
+    request.session.regenerate;
+    delete request.session.login;
+    response.send(JSON.stringify({ "ok": true }));
+});
+app.route("/api/v1/register")
+    .post(function (request, response) {
+    let data = request.body;
+    dbList.findOne({ login: data.login }).then(user => {
+        if (!user) {
+            request.session.login = data.login;
+            dbList.insertOne({
+                login: data.login,
+                password: data.password
+            }).then(() => {
+                response.send(JSON.stringify({ "ok": true }));
+            });
+        }
+        else {
+            response.status(403).send(JSON.stringify({ "error": "User is already exist!" }));
+        }
     });
 });
--- 2.4/src/index.ts
+++ 2.4/src/index.ts
@@ -1,13 +1,21 @@
-import { text } from 'body-parser';
 import express from 'express';
 import { Collection, Document, MongoClient } from 'mongodb';
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+
+declare module 'express-session' {
+  interface Session {
+    login?: string
+  }
+}
 
 
 const app = express();
 const PORT = 5001;
-const URI = "mongodb+srv://Sergey:Passw123@cluster0.4zcsikv.mongodb.net/Todo?retryWrites=true&w=majority";
-let lastID = 1;
+const URI = "mongodb+srv://Sergey:@cluster0.4zcsikv.mongodb.net/Todo?retryWrites=true&w=majority";
+
 
 let dbList: Collection<Document>; //data base with to do list
 
@@ -15,47 +23,126 @@ const client = new MongoClient(URI);
 
 //connectin to db
 client.connect()
-.then((client) => {
-  dbList = client.db("Todo").collection("ToDoList");
-  console.log("Connected to DB")
-  app.listen(PORT, () => console.log(`Running on PORT ${PORT}`)); //after db connect starts server
-})
-.catch((err) => console.log("Mongo error " + err));
+  .then((client) => {
+    dbList = client.db("Todo").collection("ToDoList");
+    console.log("Connected to DB")
+    app.listen(PORT, () => console.log(`Running on PORT ${PORT}`)); //after db connect starts server
+  })
+  .catch((err) => console.log("Mongo error " + err));
 
+
+app.use(session({
+  store: new FileStore({}),
+  key: "user_session",
+  secret: 'strong password',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 100000 },
+}));
 
 app.use(express.static(__dirname.replace("dist", "html")));
 app.use(bodyParser.json())
 
 app.route("/api/v1/items")
-  .get(function(request, response) { //get items
-    dbList.find({}, {projection: {_id: 0}}).sort({id: 1}).toArray()
-    .then((data) => {
-      lastID = data.length == 0 ? 0 : data[data.length - 1].id; //calculating id
-      response.send(JSON.stringify({ items : data}));
+  .get(function (request, response) { //get items
+    dbList.findOne({ login: request.session.login }, { projection: { _id: 0 } }).then(user => {
+      if (user && user.items) {
+        response.send(JSON.stringify({ items: user.items }));
+      } else {
+        response.send(JSON.stringify({ items: [] }));
+      }
     })
   })
-  .post(function(request, response) { //post item
+
+  .post(async function (request, response) { //post item
+    let data: string = request.body.text;
+    let id: number;
+    let user = await dbList.findOne({ login: request.session.login })
+    if (user) {
+      if (user.items ) {
+        id = user.items.reduce((acc: { id: number }, curr: { id: number }) => acc.id > curr.id ? acc : curr);
+      } else {
+        id = 1;
+      }
+    } else {
+      response.status(400).send(JSON.stringify({ "error": "User is not exist!" }));
+      return
+    }
+    dbList.updateOne({ login: request.session.login },
+      {
+        $push: {
+          "items": {
+            "id": id,
+            "text": data,
+            "checked": false
+          }
+        }
+      }).then((res) => {
+        response.send(JSON.stringify({ id: id }));
+      })
+  })
+
+  .put(function (request, response) { // change item
     let data = request.body
-    dbList.insertOne({
-      id: ++lastID,
-      text: data.text,
-      checked: false
+    dbList.updateOne({ login: request.session.login, "items.id": data.id }, {
+      $set: {
+        "items.$.text": data.text,
+        "items.$.checked": data.checked
+      }
     }).then((res) => {
-      response.send(JSON.stringify({ id : lastID}));
+      response.send(JSON.stringify({ "ok": true }));
     })
   })
-  .put(function(request, response) { // change item
-    let data = request.body
-    dbList.findOneAndUpdate({id : data.id},{$set:{
-      text: data.text,
-      checked: data.checked
-    }}).then((res) => {
-      response.send(JSON.stringify({ "ok" : Boolean(res.ok) }));
+
+  .delete(function (request, response) { //delete item
+    dbList.updateOne({ login: request.session.login }, {
+      $unset: {
+        items: {
+          id: request.body.id
+        }
+      }
+    })
+      .then((res) => {
+        response.send(JSON.stringify({ "ok": true }));
+      })
+  })
+
+
+app.route("/api/v1/login")
+  .post(function (request, response) {
+    let data = request.body;
+    dbList.findOne({ login: data.login }).then(user => {
+      if (user) {
+        request.session.login = data.login;
+        response.send(JSON.stringify({ "ok": true }));
+      } else {
+        response.status(403).send(JSON.stringify({ "error": "User is not exist!" }));
+      }
     })
   })
-  .delete(function(request, response) { //delete item
-    dbList.deleteOne({id: request.body.id})
-    .then((res) => {
-      response.send(JSON.stringify({ "ok" : Boolean(res.deletedCount) }));
+
+app.route("/api/v1/logout")
+  .post(function (request, response) {
+    request.session.destroy;
+    request.session.regenerate;
+    delete request.session.login;
+    response.send(JSON.stringify({ "ok": true }));
+  })
+
+app.route("/api/v1/register")
+  .post(function (request, response) {
+    let data = request.body;
+    dbList.findOne({ login: data.login }).then(user => {
+      if (!user) {
+        request.session.login = data.login;
+        dbList.insertOne({
+          login: data.login,
+          password: data.password
+        }).then(() => {
+          response.send(JSON.stringify({ "ok": true }));
+        })
+      } else {
+        response.status(403).send(JSON.stringify({ "error": "User is already exist!" }));
+      }
     })
   })
